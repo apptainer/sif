ec7abef074e343f99047d1bccf3f3e055468b6b1
Yannick Cote <yhcote@gmail.com>
2018-06-09 01:23:05 -0700

    Use better naming convention.
    Signed-off-by: Yannick Cote <yhcote@gmail.com>

---
+++ pkg/sif/create.go
@@ -0,0 +1,246 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017, SingularityWare, LLC. All rights reserved.
+// Copyright (c) 2017, Yannick Cote <yhcote@gmail.com> All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package sif
+
+import (
+	"bytes"
+	"container/list"
+	"encoding/binary"
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/satori/go.uuid"
+	"io"
+	"os"
+	"os/user"
+	"path"
+	"strconv"
+	"time"
+)
+
+// Find next offset aligned to block size
+func nextAligned(offset int64, align int) int64 {
+	align64 := uint64(align)
+	offset64 := uint64(offset)
+
+	if offset64%align64 != 0 {
+		offset64 = (offset64 & ^(align64 - 1)) + align64
+	}
+
+	return int64(offset64)
+}
+
+// Set file pointer offset to next aligned block
+func setFileOffNA(fimg *FileImage, alignment int) (int64, error) {
+	offset, err := fimg.fp.Seek(0, 1) // get current position
+	if err != nil {
+		return -1, fmt.Errorf("seek() getting current file position: %s", err)
+	}
+	aligned := nextAligned(offset, alignment)
+	offset, err = fimg.fp.Seek(aligned, 0) // set new position
+	if err != nil {
+		return -1, fmt.Errorf("seek() getting current file position: %s", err)
+	}
+	return offset, nil
+}
+
+// Get current user and returns both uid and gid
+func getUserIDs() (int64, int64, error) {
+	u, err := user.Current()
+	if err != nil {
+		return -1, -1, fmt.Errorf("getting current user info: %s", err)
+	}
+
+	uid, err := strconv.Atoi(u.Uid)
+	if err != nil {
+		return -1, -1, fmt.Errorf("converting UID: %s", err)
+	}
+
+	gid, err := strconv.Atoi(u.Gid)
+	if err != nil {
+		return -1, -1, fmt.Errorf("converting GID: %s", err)
+	}
+
+	return int64(uid), int64(gid), nil
+}
+
+// Fill all of the fields of a Descriptor
+func fillDescriptor(fimg *FileImage, descr *Descriptor, input descriptorInput) (err error) {
+	descr.datatype = input.datatype
+	descr.id = uuid.NewV4()
+	descr.used = true
+	descr.groupid = input.groupid
+	descr.link = input.link
+	descr.fileoff, err = setFileOffNA(fimg, os.Getpagesize())
+	if err != nil {
+		return
+	}
+	descr.filelen = input.size
+	descr.ctime = time.Now().Unix()
+	descr.mtime = time.Now().Unix()
+	descr.uid, descr.gid, err = getUserIDs()
+	if err != nil {
+		return fmt.Errorf("filling descriptor: %s", err)
+	}
+	copy(descr.name[:DescrNameLen], path.Base(input.fname))
+	copy(descr.private[:DescrMaxPrivLen], input.extra.Bytes())
+
+	glog.Infoln(descr)
+	glog.Flush()
+
+	return
+}
+
+// Write new data object to the SIF file
+func writeDataObject(fimg *FileImage, input descriptorInput) error {
+	if n, err := io.Copy(fimg.fp, input.fp); err != nil {
+		return fmt.Errorf("copying data object file to SIF file: %s", err)
+	} else if n != input.size {
+		return fmt.Errorf("short write while copying data object file to SIF file")
+	}
+	return nil
+}
+
+// Find a free descriptor and create a memory representation for addition to the SIF file
+func createDescriptor(fimg *FileImage, descrtable *[DescrNumEntries]Descriptor, e *list.Element) (err error) {
+	var (
+		idx int
+		v   Descriptor
+	)
+
+	// look for a free entry in the descriptor table
+	for idx, v = range descrtable {
+		if v.used == false {
+			break
+		}
+	}
+	if idx == DescrNumEntries-1 && descrtable[idx].used == true {
+		return fmt.Errorf("no descriptor table free entry")
+	}
+
+	// extract the descriptor input info from the list element
+	input, ok := e.Value.(descriptorInput)
+	if ok == false {
+		return fmt.Errorf("structure is not of expected descriptorInput type")
+	}
+
+	// fill in SIF file descriptor
+	if err = fillDescriptor(fimg, &descrtable[idx], input); err != nil {
+		return
+	}
+
+	// write data object associated to the descriptor in SIF file
+	if err = writeDataObject(fimg, input); err != nil {
+		return fmt.Errorf("writing data object for SIF file: %s", err)
+	}
+
+	return
+}
+
+// Release and write the data object descriptor to backing storage (SIF container file)
+func writeDescriptors(fimg *FileImage, descrtable *[DescrNumEntries]Descriptor) error {
+	buf := new(bytes.Buffer)
+	for _, v := range descrtable {
+		if err := binary.Write(buf, binary.LittleEndian, v); err != nil {
+			return fmt.Errorf("binary writing descrtable to buf: %s", err)
+		}
+	}
+	fimg.header.descrlen = int64(binary.Size(descrtable))
+
+	// first, move to descriptor start offset
+	if _, err := fimg.fp.Seek(DescrStartOffset, 0); err != nil {
+		return fmt.Errorf("seeking to descriptor start offset: %s", err)
+	}
+
+	if n, err := fimg.fp.Write(buf.Bytes()); err != nil || n < buf.Len() {
+		if err != nil {
+			return fmt.Errorf("writing descriptor table failed: %s", err)
+		}
+		return fmt.Errorf("writing descriptor table, short write")
+	}
+
+	return nil
+}
+
+// Write the global header to file
+func writeHeader(fimg *FileImage) error {
+	buf := new(bytes.Buffer)
+	if err := binary.Write(buf, binary.LittleEndian, fimg.header); err != nil {
+		return fmt.Errorf("binary writing header to buf: %s", err)
+	}
+
+	// first, move to descriptor start offset
+	if _, err := fimg.fp.Seek(0, 0); err != nil {
+		return fmt.Errorf("seeking to descriptor start offset: %s", err)
+	}
+
+	if n, err := fimg.fp.Write(buf.Bytes()); err != nil || n < buf.Len() {
+		if err != nil {
+			return fmt.Errorf("writing global header failed: %s", err)
+		}
+		return fmt.Errorf("writing global header, short write")
+	}
+
+	return nil
+}
+
+// CreateContainer is responsible for the creation of a new SIF container
+// file. It takes the creation information specification as input
+// and produces an output file as specified in the input data.
+func CreateContainer(cinfo CreateInfo) (err error) {
+	var fimg FileImage
+	var descrtable [DescrNumEntries]Descriptor
+
+	if cinfo.inputlist.Len() == 0 {
+		return fmt.Errorf("need at least one input descriptor")
+	}
+
+	// Prepare a fresh global header
+	copy(fimg.header.launch[:], cinfo.launchstr)
+	copy(fimg.header.magic[:], HdrMagic)
+	copy(fimg.header.version[:], cinfo.sifversion)
+	copy(fimg.header.arch[:], cinfo.arch)
+	copy(fimg.header.id[:], cinfo.id[:])
+	fimg.header.ctime = time.Now().Unix()
+	fimg.header.mtime = time.Now().Unix()
+	fimg.header.dfree = DescrNumEntries
+	fimg.header.dtotal = DescrNumEntries
+	fimg.header.descroff = DescrStartOffset
+	fimg.header.dataoff = DataStartOffset
+
+	fimg.nextid = 1
+
+	// Create container file
+	fimg.fp, err = os.OpenFile(cinfo.pathname, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		return fmt.Errorf("container file creation failed: %s", err)
+	}
+	defer fimg.fp.Close()
+
+	// set file pointer to start of data section */
+	if _, err = fimg.fp.Seek(DataStartOffset, 0); err != nil {
+		return fmt.Errorf("setting file offset pointer to DataStartOffset: %s", err)
+	}
+
+	for e := cinfo.inputlist.Front(); e != nil; e = e.Next() {
+		if err = createDescriptor(&fimg, &descrtable, e); err != nil {
+			return
+		}
+	}
+
+	// Write down the descriptor array
+	if err = writeDescriptors(&fimg, &descrtable); err != nil {
+		return
+	}
+
+	// Write down header file
+	if err = writeHeader(&fimg); err != nil {
+		return
+	}
+
+	return
+}
--- pkg/sif/sif.go
+++ pkg/sif/sif.go
@@ -5,242 +5,301 @@
 // LICENSE file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
+// Package sif implements data structures and routines to create
+// and access SIF files. sifdata.go contains the data definition
+// the file format. sif.go implements the core functionality of the
+// file image format. sifaccess.go implements mostly search routines
+// and access to specific descriptor/data found in SIF container files.
 package sif
 
 import (
 	"bytes"
 	"container/list"
-	"encoding/binary"
-	"fmt"
-	"github.com/golang/glog"
 	"github.com/satori/go.uuid"
-	"io"
 	"os"
-	"os/user"
-	"path"
-	"strconv"
-	"time"
 )
 
-// Find next offset aligned to block size
-func nextAligned(offset int64, align int) int64 {
-	align64 := uint64(align)
-	offset64 := uint64(offset)
+// Layout of a SIF file (example)
+//
+// .================================================.
+// | GLOBAL HEADER: Sifheader                       |
+// | - launch: "#!/usr/bin/env..."                  |
+// | - magic: "SIF_MAGIC"                           |
+// | - version: "1"                                 |
+// | - arch: "4"                                    |
+// | - uuid: b2659d4e-bd50-4ea5-bd17-eec5e54f918e   |
+// | - ctime: 1504657553                            |
+// | - mtime: 1504657653                            |
+// | - ndescr: 3                                    |
+// | - descroff: 120                                | --.
+// | - descrlen: 432                                |   |
+// | - dataoff: 4096                                |   |
+// | - datalen: 619362                              |   |
+// |------------------------------------------------| <-'
+// | DESCR[0]: Sifdeffile                           |
+// | - Sifcommon                                    |
+// |   - datatype: DATA_DEFFILE                     |
+// |   - id: 1                                      |
+// |   - groupid: 1                                 |
+// |   - link: NONE                                 |
+// |   - fileoff: 4096                              | --.
+// |   - filelen: 222                               |   |
+// |------------------------------------------------| <-----.
+// | DESCR[1]: Sifpartition                         |   |   |
+// | - Sifcommon                                    |   |   |
+// |   - datatype: DATA_PARTITION                   |   |   |
+// |   - id: 2                                      |   |   |
+// |   - groupid: 1                                 |   |   |
+// |   - link: NONE                                 |   |   |
+// |   - fileoff: 4318                              | ----. |
+// |   - filelen: 618496                            |   | | |
+// | - fstype: Squashfs                             |   | | |
+// | - parttype: System                             |   | | |
+// | - content: Linux                               |   | | |
+// |------------------------------------------------|   | | |
+// | DESCR[2]: Sifsignature                         |   | | |
+// | - Sifcommon                                    |   | | |
+// |   - datatype: DATA_SIGNATURE                   |   | | |
+// |   - id: 3                                      |   | | |
+// |   - groupid: NONE                              |   | | |
+// |   - link: 2                                    | ------'
+// |   - fileoff: 622814                            | ------.
+// |   - filelen: 644                               |   | | |
+// | - hashtype: SHA384                             |   | | |
+// | - entity: @                                    |   | | |
+// |------------------------------------------------| <-' | |
+// | Definition file data                           |     | |
+// | .                                              |     | |
+// | .                                              |     | |
+// | .                                              |     | |
+// |------------------------------------------------| <---' |
+// | File system partition image                    |       |
+// | .                                              |       |
+// | .                                              |       |
+// | .                                              |       |
+// |------------------------------------------------| <-----'
+// | Signed verification data                       |
+// | .                                              |
+// | .                                              |
+// | .                                              |
+// `================================================'
 
-	if offset64%align64 != 0 {
-		offset64 = (offset64 & ^(align64 - 1)) + align64
-	}
+// SIF header constants and quantities
+const (
+	HdrLaunch      = "#!/usr/bin/env run-singularity\n"
+	HdrMagic       = "SIF_MAGIC" // SIF identification
+	HdrVersion     = "0"         // SIF SPEC VERSION
+	HdrArch386     = "2"         // 386 arch code
+	HdrArchAMD64   = "4"         // AMD64 arch code
+	HdrArchARM     = "8"         // ARM arch code
+	HdrArchAARCH64 = "16"        // AARCH64 arch code
 
-	return int64(offset64)
+	HdrLaunchLen  = 32 // len("#!/usr/bin/env... ")
+	HdrMagicLen   = 10 // len("SIF_MAGIC")
+	HdrVersionLen = 3  // len("99")
+	HdrArchLen    = 3  // len("99")
+
+	DescrNumEntries   = 48                 // the default total number of available descriptors
+	DescrGroupMask    = 0xf0000000         // groups start at that offset
+	DescrUnusedGroup  = DescrGroupMask     // descriptor without a group
+	DescrDefaultGroup = DescrGroupMask | 1 // first groupid number created
+	DescrUnusedLink   = 0                  // descriptor without link to other
+	DescrEntityLen    = 256                // len("Joe Bloe <jbloe@gmail.com>...")
+	DescrNameLen      = 128                // descriptor name (string identifier)
+	DescrMaxPrivLen   = 384                // size reserved for descriptor specific data
+	DescrStartOffset  = 4096               // where descriptors start after global header
+	DataStartOffset   = 32768              // where data object start after descriptors
+)
+
+// Datatype represents the different SIF data object types stored in the image
+type Datatype int32
+
+// List of supported SIF data types
+const (
+	DataDeffile     Datatype = iota + 0x4001 // definition file data object
+	DataEnvVar                               // environment variables data object
+	DataLabels                               // JSON labels data object
+	DataPartition                            // file system data object
+	DataSignature                            // signing/verification data object
+	DataGenericJSON                          // generic JSON meta-data
+)
+
+// Fstype represents the different SIF file system types found in partition data objects
+type Fstype int32
+
+// List of supported file systems
+const (
+	FsSquash  Fstype = iota + 1 // Squashfs file system, RDONLY
+	FsExt3                      // EXT3 file system, RDWR (deprecated)
+	FsImmuObj                   // immutable data object archive
+	FsRaw                       // raw data
+)
+
+// Parttype represents the different SIF container partition types (system and data)
+type Parttype int32
+
+// List of supported partition types
+const (
+	PartSystem  Parttype = iota + 1 // partition hosts an operating system
+	PartData                        // partition hosts data only
+	PartOverlay                     // partition hosts an overlay
+)
+
+// Hashtype represents the different SIF hashing function types used to fingerprint data objects
+type Hashtype int32
+
+// List of supported hash functions
+const (
+	HashSHA256 Hashtype = iota + 1
+	HashSHA384
+	HashSHA512
+	HashBLAKE2S
+	HashBLAKE2B
+)
+
+// SIF data object deletation strategies
+const (
+	DelZero    = iota + 1 // zero the data object bytes
+	DelCompact            // free the space used by data object
+)
+
+// Deffile represets the SIF definition-file data object descriptor
+type Deffile struct {
 }
 
-// Set file pointer offset to next aligned block
-func setFileOffNA(fimg *FileImage, alignment int) (int64, error) {
-	offset, err := fimg.fp.Seek(0, 1) // get current position
-	if err != nil {
-		return -1, fmt.Errorf("seek() getting current file position: %s", err)
-	}
-	aligned := nextAligned(offset, alignment)
-	offset, err = fimg.fp.Seek(aligned, 0) // set new position
-	if err != nil {
-		return -1, fmt.Errorf("seek() getting current file position: %s", err)
-	}
-	return offset, nil
+// Labels represents the SIF JSON-labels data object descriptor
+type Labels struct {
 }
 
-// Get current user and returns both uid and gid
-func getUserIDs() (int64, int64, error) {
-	u, err := user.Current()
-	if err != nil {
-		return -1, -1, fmt.Errorf("getting current user info: %s", err)
-	}
-
-	uid, err := strconv.Atoi(u.Uid)
-	if err != nil {
-		return -1, -1, fmt.Errorf("converting UID: %s", err)
-	}
-
-	gid, err := strconv.Atoi(u.Gid)
-	if err != nil {
-		return -1, -1, fmt.Errorf("converting GID: %s", err)
-	}
-
-	return int64(uid), int64(gid), nil
+// Envvar represents the SIF envvar data object descriptor
+type Envvar struct {
 }
 
-// Fill all of the fields of a Descriptor
-func fillDescriptor(fimg *FileImage, descr *Descriptor, input descriptorInput) (err error) {
-	descr.datatype = input.datatype
-	descr.id = uuid.NewV4()
-	descr.used = true
-	descr.groupid = input.groupid
-	descr.link = input.link
-	descr.fileoff, err = setFileOffNA(fimg, os.Getpagesize())
-	if err != nil {
-		return
-	}
-	descr.filelen = input.size
-	descr.ctime = time.Now().Unix()
-	descr.mtime = time.Now().Unix()
-	descr.uid, descr.gid, err = getUserIDs()
-	if err != nil {
-		return fmt.Errorf("filling descriptor: %s", err)
-	}
-	copy(descr.name[:DescrNameLen], path.Base(input.fname))
-	copy(descr.private[:DescrMaxPrivLen], input.extra.Bytes())
-
-	glog.Infoln(descr)
-	glog.Flush()
-
-	return
+// Partition represents the SIF partition data object descriptor
+type Partition struct {
+	fstype   Fstype
+	parttype Parttype
 }
 
-// Write new data object to the SIF file
-func writeDataObject(fimg *FileImage, input descriptorInput) error {
-	if n, err := io.Copy(fimg.fp, input.fp); err != nil {
-		return fmt.Errorf("copying data object file to SIF file: %s", err)
-	} else if n != input.size {
-		return fmt.Errorf("short write while copying data object file to SIF file")
-	}
-	return nil
+// Signature represents the SIF signature data object descriptor
+type Signature struct {
+	hashtype Hashtype
+	entity   [DescrEntityLen]byte
 }
 
-// Find a free descriptor and create a memory representation for addition to the SIF file
-func createDescriptor(fimg *FileImage, descrtable *[DescrNumEntries]Descriptor, e *list.Element) (err error) {
-	var (
-		idx int
-		v   Descriptor
-	)
-
-	// look for a free entry in the descriptor table
-	for idx, v = range descrtable {
-		if v.used == false {
-			break
-		}
-	}
-	if idx == DescrNumEntries-1 && descrtable[idx].used == true {
-		return fmt.Errorf("no descriptor table free entry")
-	}
-
-	// extract the descriptor input info from the list element
-	input, ok := e.Value.(descriptorInput)
-	if ok == false {
-		return fmt.Errorf("structure is not of expected descriptorInput type")
-	}
-
-	// fill in SIF file descriptor
-	if err = fillDescriptor(fimg, &descrtable[idx], input); err != nil {
-		return
-	}
-
-	// write data object associated to the descriptor in SIF file
-	if err = writeDataObject(fimg, input); err != nil {
-		return fmt.Errorf("writing data object for SIF file: %s", err)
-	}
-
-	return
+// GenericJSON represents the SIF generic JSON meta-data data object descriptor
+type GenericJSON struct {
 }
 
-// Release and write the data object descriptor to backing storage (SIF container file)
-func writeDescriptors(fimg *FileImage, descrtable *[DescrNumEntries]Descriptor) error {
-	buf := new(bytes.Buffer)
-	for _, v := range descrtable {
-		if err := binary.Write(buf, binary.LittleEndian, v); err != nil {
-			return fmt.Errorf("binary writing descrtable to buf: %s", err)
-		}
-	}
-	fimg.header.descrlen = int64(binary.Size(descrtable))
+// Descriptor represents the SIF descriptor type
+type Descriptor struct {
+	datatype Datatype  // informs of descriptor type
+	id       uuid.UUID // a unique id for this data object
+	used     bool      // is the descriptor in use
+	groupid  uint32    // object group this data object is related to
+	link     uint32    // special link or relation to an id or group
+	fileoff  int64     // offset from start of image file
+	filelen  int64     // length of data in file
 
-	// first, move to descriptor start offset
-	if _, err := fimg.fp.Seek(DescrStartOffset, 0); err != nil {
-		return fmt.Errorf("seeking to descriptor start offset: %s", err)
-	}
-
-	if n, err := fimg.fp.Write(buf.Bytes()); err != nil || n < buf.Len() {
-		if err != nil {
-			return fmt.Errorf("writing descriptor table failed: %s", err)
-		}
-		return fmt.Errorf("writing descriptor table, short write")
-	}
-
-	return nil
+	ctime   int64                 // image creation time
+	mtime   int64                 // last modification time
+	uid     int64                 // system user owning the file
+	gid     int64                 // system group owning the file
+	name    [DescrNameLen]byte    // descriptor name (string identifier)
+	private [DescrMaxPrivLen]byte // big enough for above extra data
 }
 
-// Write the global header to file
-func writeHeader(fimg *FileImage) error {
-	buf := new(bytes.Buffer)
-	if err := binary.Write(buf, binary.LittleEndian, fimg.header); err != nil {
-		return fmt.Errorf("binary writing header to buf: %s", err)
-	}
+// Header describes a loaded SIF file
+type Header struct {
+	launch [HdrLaunchLen]byte // #! shell execution line
 
-	// first, move to descriptor start offset
-	if _, err := fimg.fp.Seek(0, 0); err != nil {
-		return fmt.Errorf("seeking to descriptor start offset: %s", err)
-	}
+	magic   [HdrMagicLen]byte   // look for "SIF_MAGIC"
+	version [HdrVersionLen]byte // SIF version
+	arch    [HdrArchLen]byte    // arch the image is built for
+	id      uuid.UUID           // image unique identifier
 
-	if n, err := fimg.fp.Write(buf.Bytes()); err != nil || n < buf.Len() {
-		if err != nil {
-			return fmt.Errorf("writing global header failed: %s", err)
-		}
-		return fmt.Errorf("writing global header, short write")
-	}
+	ctime int64 // image creation time
+	mtime int64 // last modification time
 
-	return nil
+	dfree    int64 // # of used data object descr.
+	dtotal   int64 // # of total available data object descr.
+	descroff int64 // bytes into file where descs start
+	descrlen int64 // bytes used by all current descriptors
+	dataoff  int64 // bytes into file where data starts
+	datalen  int64 // bytes used by all data objects
 }
 
-// CreateContainer is responsible for the creation of a new SIF container
-// file. It takes the creation information specification as input
-// and produces an output file as specified in the input data.
-func CreateContainer(cinfo CreateInfo) (err error) {
-	var fimg FileImage
-	var descrtable [DescrNumEntries]Descriptor
+// FileImage describes the representation of a SIF file in memory
+type FileImage struct {
+	header    Header     // the loaded SIF global header
+	nextid    int        // the next id to use for new descriptors
+	fp        *os.File   // file pointer of opened SIF file
+	filesize  int64      // file size of the opened SIF file
+	filedata  []byte     // the content of the opened file
+	descrlist *list.List // list of loaded descriptors from SIF file
+}
 
-	if cinfo.inputlist.Len() == 0 {
-		return fmt.Errorf("need at least one input descriptor")
-	}
+// CreateInfo wraps all SIF file creation info needed
+type CreateInfo struct {
+	pathname   string     // the end result output filename
+	launchstr  string     // the shell run command
+	sifversion string     // the SIF specification version used
+	arch       string     // the architecture targetted
+	id         uuid.UUID  // image unique identifier
+	inputlist  *list.List // list head of input info for descriptor creation
+}
 
-	// Prepare a fresh global header
-	copy(fimg.header.launch[:], cinfo.launchstr)
-	copy(fimg.header.magic[:], HdrMagic)
-	copy(fimg.header.version[:], cinfo.sifversion)
-	copy(fimg.header.arch[:], cinfo.arch)
-	copy(fimg.header.id[:], cinfo.id[:])
-	fimg.header.ctime = time.Now().Unix()
-	fimg.header.mtime = time.Now().Unix()
-	fimg.header.dfree = DescrNumEntries
-	fimg.header.dtotal = DescrNumEntries
-	fimg.header.descroff = DescrStartOffset
-	fimg.header.dataoff = DataStartOffset
+//
+// This section describes SIF creation data structures used when building
+// a new SIF file. Transient data not found in the final SIF file. Those data
+// structures are internal.
+//
 
-	fimg.nextid = 1
+// descriptorInput describes the common info needed to create a data object descriptor
+type descriptorInput struct {
+	datatype Datatype // datatype being harvested for new descriptor
+	groupid  uint32   // group to be set for new descriptor
+	link     uint32   // link to be set for new descriptor
+	size     int64    // size of the data object for the new descriptor
 
-	// Create container file
-	fimg.fp, err = os.OpenFile(cinfo.pathname, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	if err != nil {
-		return fmt.Errorf("container file creation failed: %s", err)
-	}
-	defer fimg.fp.Close()
+	fname string   // file containing data associated with the new descriptor
+	fp    *os.File // file pointer to opened 'fname'
+	data  []byte   // loaded data from file
 
-	// set file pointer to start of data section */
-	if _, err = fimg.fp.Seek(DataStartOffset, 0); err != nil {
-		return fmt.Errorf("setting file offset pointer to DataStartOffset: %s", err)
-	}
+	image *FileImage  // loaded SIF file in memory
+	descr *Descriptor // created end result descriptor
 
-	for e := cinfo.inputlist.Front(); e != nil; e = e.Next() {
-		if err = createDescriptor(&fimg, &descrtable, e); err != nil {
-			return
-		}
-	}
+	extra bytes.Buffer // where specific input type store their data
+}
 
-	// Write down the descriptor array
-	if err = writeDescriptors(&fimg, &descrtable); err != nil {
-		return
-	}
+// defInput describes the info needed to create an definition-file descriptor
+type defInput struct {
+	// nothing specific for definition-file yet
+}
 
-	// Write down header file
-	if err = writeHeader(&fimg); err != nil {
-		return
-	}
+// envInput describes the info needed to create an env. var. descriptor
+type envInput struct {
+	// nothing specific for env. var. yet
+}
 
-	return
+// labelInput describes the info needed to create an label descriptor
+type labelInput struct {
+	// nothing specific for label yet
+}
+
+// partInput describes the info needed to create an partition descriptor
+type partInput struct {
+	Fstype   Fstype
+	Parttype Parttype
+}
+
+// sigInput describes the info needed to create an signature descriptor
+type sigInput struct {
+	Hashtype Hashtype
+	Entity   [DescrEntityLen]byte
+}
+
+// genJInput describes the info needed to create an generic JSON meta-data descriptor
+type genJInput struct {
+	// nothing specific for generic JSON meta-data yet
 }
