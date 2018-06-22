0c10230b719a787da86d3b71896616400f0aac9d
Yannick Cote <yhcote@gmail.com>
2018-06-22 04:52:17 -0700

    Start working on the siftool program.
    Signed-off-by: Yannick Cote <yhcote@gmail.com>

---
+++ cmd/siftool/doc.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+/*
+Siftool is a program for Singularity Image Format (SIF) file manipulation.
+
+A set of commands are provided to display elements such as the SIF global
+header, the data object descriptors and to dump data objects. It is also
+possible to modify a SIF file via this tool via the add/del commands.
+*/
+package main
---
+++ cmd/siftool/siftool.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017, SingularityWare, LLC. All rights reserved.
+// Copyright (c) 2017, Yannick Cote <yhcote@gmail.com> All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const usageMessage = "" +
+	`Usage of 'siftool':
+`
+
+func usage() {
+	fmt.Fprintln(os.Stderr, usageMessage)
+	fmt.Fprintln(os.Stderr, "Flags:")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+}
