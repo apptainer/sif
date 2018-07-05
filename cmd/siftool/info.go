a8a4f6aa8a1f4c8f359438641266ec6f73a6ff1e
Divya Cote <divya.cote@gmail.com>
2018-07-04 23:41:32 -0700

    Start implementing the info category of commands.
    Signed-off-by: Divya Cote <divya.cote@gmail.com>

---
+++ cmd/siftool/info.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2018, Divya Cote <divya.cote@gmail.com> All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package main
+
+// cmdHeader displays the SIF file global header
+func cmdHeader(args []string) error {
+	return nil
+}
+
+// cmdList lists basic information of all currently used descriptors
+func cmdList(args []string) error {
+	return nil
+}
+
+// cmdInfo give detailed information about a specific descriptor
+func cmdInfo(args []string) error {
+	return nil
+}
+
+// cmdDump extracts a data object from a SIF file and outputs it to stdout
+func cmdDump(args []string) error {
+	return nil
+}
