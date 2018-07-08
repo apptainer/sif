f48697e19f27b1c522b53de65dfa8952cc702020
Yannick Cote <yhcote@gmail.com>
2018-07-08 14:04:38 -0700

    add usage/help and plug commands correctly.
    Signed-off-by: Yannick Cote <yhcote@gmail.com>

--- cmd/siftool/siftool.go
+++ cmd/siftool/siftool.go
@@ -14,13 +14,43 @@ import (
 	"os"
 )
 
-const usageMessage = "" +
-	`Usage of 'siftool':
+var usageMessage = `siftool is a utility program for manipulating SIF files.
+
+Usage:
+
+	siftool command [arguments]
+
+The commands are:
+
+	header   display SIF global headers
+	list     list object descriptors from SIF files
+	info     display detailed information of object descriptors
+	dump     extract and output (stdout) data objects from SIF files
+	del      delete a specified object descriptor and data from SIF file
+`
+
+const usageHeader = "" +
+	`usage: header containerfile
+`
+
+const usageList = "" +
+	`usage: list containerfile
+`
+
+const usageInfo = "" +
+	`usage: info descriptorid containerfile
+`
+
+const usageDump = "" +
+	`usage: dump descriptorid containerfile
+`
+
+const usageDel = "" +
+	`usage: del descriptorid containerfile
 `
 
 func usage() {
 	fmt.Fprintln(os.Stderr, usageMessage)
-	fmt.Fprintln(os.Stderr, "Flags:")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
@@ -40,27 +70,47 @@ func main() {
 	case "header":
 		err := cmdHeader(args[1:])
 		if err != nil {
-			log.Fatal("error running `header' command:", err)
+			if err.Error() == "usage" {
+				log.Fatal(usageHeader)
+			} else {
+				log.Fatal("error running `header' command:", err)
+			}
 		}
 	case "list":
 		err := cmdList(args[1:])
 		if err != nil {
-			log.Fatal("error running `list' command:", err)
+			if err.Error() == "usage" {
+				log.Fatal(usageList)
+			} else {
+				log.Fatal("error running `list' command:", err)
+			}
 		}
 	case "info":
 		err := cmdInfo(args[1:])
 		if err != nil {
-			log.Fatal("error running `info' command:", err)
+			if err.Error() == "usage" {
+				log.Fatal(usageInfo)
+			} else {
+				log.Fatal("error running `info' command:", err)
+			}
 		}
 	case "dump":
 		err := cmdDump(args[1:])
 		if err != nil {
-			log.Fatal("error running `dump' command:", err)
+			if err.Error() == "usage" {
+				log.Fatal(usageDump)
+			} else {
+				log.Fatal("error running `dump' command:", err)
+			}
 		}
 	case "del":
 		err := cmdDel(args[1:])
 		if err != nil {
-			log.Fatal("error running `del' command:", err)
+			if err.Error() == "usage" {
+				log.Fatal(usageDel)
+			} else {
+				log.Fatal("error running `del' command:", err)
+			}
 		}
 	default:
 		log.Fatal("Unknown command:", args[0])
