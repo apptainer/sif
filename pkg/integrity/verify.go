2b7a0bf59c5cf8fbbaa118ed0fc90a471dbbf5b7
Adam Hughes <stickmanica@gmail.com>
2020-05-28 00:14:07 +0000

    Add Verifier implementation

---
+++ pkg/integrity/verify.go
@@ -0,0 +1,100 @@
+// Copyright (c) 2020, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the LICENSE.md file
+// distributed with the sources of this project regarding your rights to use or distribute this
+// software.
+
+package integrity
+
+import (
+	"fmt"
+
+	"github.com/sylabs/sif/pkg/sif"
+	"golang.org/x/crypto/openpgp"
+)
+
+// Verifier describes a SIF image verifier.
+type Verifier struct {
+	f *sif.FileImage // SIF image to verify.
+
+	keyRing  openpgp.KeyRing // Keyring to use for verification.
+	groups   []uint32        // Data object group(s) selected for verification.
+	objects  []uint32        // Individual data object(s) selected for verification.
+	isLegacy bool            // Enable verification of legacy signature(s).
+}
+
+// VerifierOpt are used to configure v.
+type VerifierOpt func(v *Verifier) error
+
+// OptVerifyWithKeyRing sets the keyring to use for verification to kr.
+func OptVerifyWithKeyRing(kr openpgp.KeyRing) VerifierOpt {
+	return func(v *Verifier) error {
+		v.keyRing = kr
+		return nil
+	}
+}
+
+// OptVerifyGroup adds a verification task for the group with the specified groupID. This may be
+// called multliple times to request verification of more than one group.
+func OptVerifyGroup(groupID uint32) VerifierOpt {
+	return func(v *Verifier) error {
+		if groupID == 0 {
+			return errInvalidGroupID
+		}
+		v.groups = insertSorted(v.groups, groupID)
+		return nil
+	}
+}
+
+// OptVerifyObject adds a verification task for the object with the specified id. This may be
+// called multliple times to request verification of more than one object.
+func OptVerifyObject(id uint32) VerifierOpt {
+	return func(v *Verifier) error {
+		if id == 0 {
+			return errInvalidObjectID
+		}
+		v.objects = insertSorted(v.objects, id)
+		return nil
+	}
+}
+
+// OptVerifyLegacy enables verification of legacy signatures. Non-legacy signatures will not be
+// considered.
+func OptVerifyLegacy() VerifierOpt {
+	return func(v *Verifier) error {
+		v.isLegacy = true
+		return nil
+	}
+}
+
+// NewVerifier returns a Verifier to examine and/or verify digital signatures(s) in f according to
+// opts.
+//
+// Verify requires key material be provided. OptVerifyWithKeyRing can be used for this purpose.
+//
+// By default, the returned Verifier will consider non-legacy signatures for all object groups. To
+// override this behavior, consider using OptVerifyGroup, OptVerifyObject, and/or OptVerifyLegacy.
+func NewVerifier(f *sif.FileImage, opts ...VerifierOpt) (*Verifier, error) {
+	if f == nil {
+		return nil, fmt.Errorf("integrity: %w", errNilFileImage)
+	}
+
+	v := &Verifier{f: f}
+
+	// Apply options.
+	for _, o := range opts {
+		if err := o(v); err != nil {
+			return nil, fmt.Errorf("integrity: %w", err)
+		}
+	}
+
+	// If no verification tasks specified, add one per object group
+	if len(v.groups) == 0 && len(v.objects) == 0 {
+		ids, err := getGroupIDs(f)
+		if err != nil {
+			return nil, fmt.Errorf("integrity: %w", err)
+		}
+		v.groups = ids
+	}
+
+	return v, nil
+}
---
+++ pkg/integrity/verify_test.go
@@ -0,0 +1,200 @@
+// Copyright (c) 2020, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the LICENSE.md file
+// distributed with the sources of this project regarding your rights to use or distribute this
+// software.
+
+package integrity
+
+import (
+	"errors"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/sylabs/sif/pkg/sif"
+	"golang.org/x/crypto/openpgp"
+)
+
+func TestNewVerifier(t *testing.T) {
+	emptyImage, err := sif.LoadContainer(filepath.Join("testdata", "images", "empty.sif"), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer emptyImage.UnloadContainer() // nolint:errcheck
+
+	oneGroupImage, err := sif.LoadContainer(filepath.Join("testdata", "images", "one-group.sif"), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer oneGroupImage.UnloadContainer() // nolint:errcheck
+
+	twoGroupImage, err := sif.LoadContainer(filepath.Join("testdata", "images", "two-groups.sif"), true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer twoGroupImage.UnloadContainer() // nolint:errcheck
+
+	kr := openpgp.EntityList{getTestEntity(t)}
+
+	tests := []struct {
+		name        string
+		fi          *sif.FileImage
+		opts        []VerifierOpt
+		wantErr     error
+		wantKeyring openpgp.KeyRing
+		wantGroups  []uint32
+		wantObjects []uint32
+		wantLegacy  bool
+	}{
+		{
+			name:    "NilFileImage",
+			fi:      nil,
+			wantErr: errNilFileImage,
+		},
+		{
+			name:        "NoGroupsFound",
+			fi:          &emptyImage,
+			opts:        []VerifierOpt{},
+			wantKeyring: kr,
+			wantErr:     errNoGroupsFound,
+		},
+		{
+			name:    "InvalidGroupID",
+			fi:      &emptyImage,
+			opts:    []VerifierOpt{OptVerifyGroup(0)},
+			wantErr: errInvalidGroupID,
+		},
+		{
+			name:    "InvalidObjectID",
+			fi:      &emptyImage,
+			opts:    []VerifierOpt{OptVerifyObject(0)},
+			wantErr: errInvalidObjectID,
+		},
+		{
+			name:       "OneGroupDefaults",
+			fi:         &oneGroupImage,
+			opts:       []VerifierOpt{},
+			wantGroups: []uint32{1},
+		},
+		{
+			name:       "TwoGroupDefaults",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{},
+			wantGroups: []uint32{1, 2},
+		},
+		{
+			name:        "OptVerifyWithKeyRing",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyWithKeyRing(kr)},
+			wantKeyring: kr,
+			wantGroups:  []uint32{1, 2},
+		},
+		{
+			name:       "OptVerifyGroupDuplicate",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyGroup(1), OptVerifyGroup(1)},
+			wantGroups: []uint32{1},
+		},
+		{
+			name:       "OptVerifyGroup1",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyGroup(1)},
+			wantGroups: []uint32{1},
+		},
+		{
+			name:       "OptVerifyGroup2",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyGroup(2)},
+			wantGroups: []uint32{2},
+		},
+		{
+			name:       "OptVerifyGroups",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyGroup(1), OptVerifyGroup(2)},
+			wantGroups: []uint32{1, 2},
+		},
+		{
+			name:        "OptVerifyObjectDuplicate",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyObject(1), OptVerifyObject(1)},
+			wantObjects: []uint32{1},
+		},
+		{
+			name:        "OptVerifyObject1",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyObject(1)},
+			wantObjects: []uint32{1},
+		},
+		{
+			name:        "OptVerifyObject2",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyObject(2)},
+			wantObjects: []uint32{2},
+		},
+		{
+			name:        "OptVerifyObject3",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyObject(3)},
+			wantObjects: []uint32{3},
+		},
+		{
+			name:        "OptVerifyObjects",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyObject(1), OptVerifyObject(2), OptVerifyObject(3)},
+			wantObjects: []uint32{1, 2, 3},
+		},
+		{
+			name:       "OptVerifyLegacy",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyLegacy()},
+			wantGroups: []uint32{1, 2},
+			wantLegacy: true,
+		},
+		{
+			name:       "OptVerifyLegacyGroup1",
+			fi:         &twoGroupImage,
+			opts:       []VerifierOpt{OptVerifyLegacy(), OptVerifyGroup(1)},
+			wantGroups: []uint32{1},
+			wantLegacy: true,
+		},
+		{
+			name:        "OptVerifyLegacyObject1",
+			fi:          &twoGroupImage,
+			opts:        []VerifierOpt{OptVerifyLegacy(), OptVerifyObject(1)},
+			wantObjects: []uint32{1},
+			wantLegacy:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			v, err := NewVerifier(tt.fi, tt.opts...)
+			if got, want := err, tt.wantErr; !errors.Is(got, want) {
+				t.Fatalf("got error %v, want %v", got, want)
+			}
+
+			if err == nil {
+				if got, want := v.f, tt.fi; got != want {
+					t.Errorf("got FileImage %v, want %v", got, want)
+				}
+
+				if got, want := v.keyRing, tt.wantKeyring; !reflect.DeepEqual(got, want) {
+					t.Errorf("got key ring %v, want %v", got, want)
+				}
+
+				if got, want := v.groups, tt.wantGroups; !reflect.DeepEqual(got, want) {
+					t.Errorf("got groups %v, want %v", got, want)
+				}
+
+				if got, want := v.objects, tt.wantObjects; !reflect.DeepEqual(got, want) {
+					t.Errorf("got objects %v, want %v", got, want)
+				}
+
+				if got, want := v.isLegacy, tt.wantLegacy; got != want {
+					t.Errorf("got legacy %v, want %v", got, want)
+				}
+			}
+		})
+	}
+}
