d209d02bdfc644d2e4f90e90566120123f904edd
Adam Hughes <stickmanica@gmail.com>
2021-06-10 15:51:13 +0000

    fix: set Amodebuf to improve backwards compatibility

--- pkg/sif/load.go
+++ pkg/sif/load.go
@@ -98,6 +98,8 @@ func LoadContainerFp(fp ReadWriter, rdonly bool) (fimg FileImage, err error) {
 	}
 	fimg.Filesize = info.Size()
 
+	fimg.Amodebuf = true // for backwards compat, true == !mmap
+
 	// read global header from SIF file
 	if err = readHeader(fp, &fimg); err != nil {
 		return
@@ -120,6 +122,8 @@ func LoadContainerFp(fp ReadWriter, rdonly bool) (fimg FileImage, err error) {
 // and extract various components like the global header, descriptors and even
 // perhaps data, depending on how much is read from the source.
 func LoadContainerReader(b *bytes.Reader) (fimg FileImage, err error) {
+	fimg.Amodebuf = true // for backwards compat, true == !mmap
+
 	// read global header from SIF file
 	if err = readHeader(b, &fimg); err != nil {
 		return
