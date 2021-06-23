dcc0b336ead7f66da0154caa08953f06e997d3d8
Adam Hughes <stickmanica@gmail.com>
2021-06-23 21:19:56 +0000

    test: add unit test for List

--- internal/app/siftool/info_test.go
+++ internal/app/siftool/info_test.go
@@ -16,6 +16,7 @@ import (
 
 var corpus = filepath.Join("..", "..", "..", "pkg", "integrity", "testdata", "images")
 
+//nolint:dupl
 func TestApp_Header(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -77,6 +78,77 @@ func TestApp_Header(t *testing.T) {
 			}
 
 			if got, want := a.Header(tt.path), tt.wantErr; !errors.Is(got, want) {
+				t.Fatalf("got error %v, want %v", got, want)
+			}
+
+			g := goldie.New(t, goldie.WithTestNameForDir(true))
+			g.Assert(t, tt.name, b.Bytes())
+		})
+	}
+}
+
+//nolint:dupl
+func TestApp_List(t *testing.T) {
+	tests := []struct {
+		name    string
+		path    string
+		wantErr error
+	}{
+		{
+			name: "Empty",
+			path: filepath.Join(corpus, "empty.sif"),
+		},
+		{
+			name: "OneGroup",
+			path: filepath.Join(corpus, "one-group.sif"),
+		},
+		{
+			name: "OneGroupSigned",
+			path: filepath.Join(corpus, "one-group-signed.sif"),
+		},
+		{
+			name: "OneGroupSignedLegacy",
+			path: filepath.Join(corpus, "one-group-signed-legacy.sif"),
+		},
+		{
+			name: "OneGroupSignedLegacyAll",
+			path: filepath.Join(corpus, "one-group-signed-legacy-all.sif"),
+		},
+		{
+			name: "OneGroupSignedLegacyGroup",
+			path: filepath.Join(corpus, "one-group-signed-legacy-group.sif"),
+		},
+		{
+			name: "TwoGroups",
+			path: filepath.Join(corpus, "two-groups.sif"),
+		},
+		{
+			name: "TwoGroupsSigned",
+			path: filepath.Join(corpus, "two-groups-signed.sif"),
+		},
+		{
+			name: "TwoGroupsSignedLegacy",
+			path: filepath.Join(corpus, "two-groups-signed-legacy.sif"),
+		},
+		{
+			name: "TwoGroupsSignedLegacyAll",
+			path: filepath.Join(corpus, "two-groups-signed-legacy-all.sif"),
+		},
+		{
+			name: "TwoGroupsSignedLegacyGroup",
+			path: filepath.Join(corpus, "two-groups-signed-legacy-group.sif"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var b bytes.Buffer
+
+			a, err := New(OptAppOutput(&b))
+			if err != nil {
+				t.Fatalf("failed to create app: %v", err)
+			}
+
+			if got, want := a.List(tt.path), tt.wantErr; !errors.Is(got, want) {
 				t.Fatalf("got error %v, want %v", got, want)
 			}
 
