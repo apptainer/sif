89586dc4a8c2e1c4fa2fcbfb79897e2e878611a2
Adam Hughes <stickmanica@gmail.com>
2021-06-25 18:33:53 +0000

    test: add unit tests for AddCommands

---
+++ pkg/siftool/siftool_test.go
@@ -0,0 +1,89 @@
+// Copyright (c) 2021, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+package siftool
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/sebdah/goldie/v2"
+	"github.com/spf13/cobra"
+)
+
+func runCommand(t *testing.T, cmd *cobra.Command, args []string) {
+	var out, err bytes.Buffer
+	cmd.SetOut(&out)
+	cmd.SetErr(&err)
+
+	cmd.SetArgs(args)
+
+	if err := cmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+
+	g := goldie.New(t,
+		goldie.WithTestNameForDir(true),
+		goldie.WithSubTestNameForDir(true),
+	)
+	g.Assert(t, "out", out.Bytes())
+	g.Assert(t, "err", err.Bytes())
+}
+
+func TestAddCommands(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+	}{
+		{
+			name: "SifTool",
+			args: []string{"help"},
+		},
+		{
+			name: "Add",
+			args: []string{"help", "add"},
+		},
+		{
+			name: "Del",
+			args: []string{"help", "del"},
+		},
+		{
+			name: "Dump",
+			args: []string{"help", "dump"},
+		},
+		{
+			name: "Header",
+			args: []string{"help", "header"},
+		},
+		{
+			name: "Info",
+			args: []string{"help", "info"},
+		},
+		{
+			name: "List",
+			args: []string{"help", "list"},
+		},
+		{
+			name: "New",
+			args: []string{"help", "new"},
+		},
+		{
+			name: "SetPrim",
+			args: []string{"help", "setprim"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := &cobra.Command{
+				Use: "siftool",
+			}
+
+			if err := AddCommands(cmd); err != nil {
+				t.Fatal(err)
+			}
+
+			runCommand(t, cmd, tt.args)
+		})
+	}
+}
