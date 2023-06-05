7e709b6cb0e08569832a9f286a7eea3d574e20df
Adam Hughes <9903835+tri-adam@users.noreply.github.com>
2023-06-05 16:14:46 +0000

    ci: enable thelper linter
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- pkg/sif/select_test.go
+++ pkg/sif/select_test.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2021, Sylabs Inc. All rights reserved.
+// Copyright (c) 2021-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -275,6 +275,7 @@ func TestFileImage_WithDescriptors(t *testing.T) {
 		{
 			name: "ReturnTrue",
 			fn: func(t *testing.T) func(d Descriptor) bool {
+				t.Helper()
 				return func(d Descriptor) bool {
 					if id := d.ID(); id > 1 {
 						t.Errorf("unexpected ID: %v", id)
@@ -286,6 +287,7 @@ func TestFileImage_WithDescriptors(t *testing.T) {
 		{
 			name: "ReturnFalse",
 			fn: func(t *testing.T) func(d Descriptor) bool {
+				t.Helper()
 				return func(d Descriptor) bool {
 					if id := d.ID(); id > 2 {
 						t.Errorf("unexpected ID: %v", id)
