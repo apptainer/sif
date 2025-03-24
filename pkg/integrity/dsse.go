d3b5cc5b2392cdeaea8dde616081b1cf6b76eb66
Adam Hughes <9903835+tri-adam@users.noreply.github.com>
2025-03-24 14:39:31 -0400

    chore: bump golangci-lint to v2.0
    Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

--- pkg/integrity/dsse.go
+++ pkg/integrity/dsse.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2022-2023, Sylabs Inc. All rights reserved.
+// Copyright (c) 2022-2025, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the LICENSE.md file
 // distributed with the sources of this project regarding your rights to use or distribute this
 // software.
@@ -127,7 +127,7 @@ type wrappedVerifier struct {
 func (wv wrappedVerifier) VerifySignature(signature, message io.Reader, opts ...signature.VerifyOption) error {
 	err := wv.Verifier.VerifySignature(signature, message, opts...)
 	if err == nil {
-		pub, err := wv.Verifier.PublicKey()
+		pub, err := wv.PublicKey()
 		if err != nil {
 			return err
 		}
